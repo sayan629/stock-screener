f88184b8beb8ca21684efcc965bf67558117357f
Sayan Pal <sayanpal771@gmail.com>
2026-09-11 21:07:41 +0530

    first commit

---
+++ flow.py
@@ -0,0 +1,13 @@
+# 1. Bring in dependencies
+# 2. Create LLM
+# 3. Create State
+# 4. Build LLM Mode
+# 5. Assemble Graph
+# 6. Add Memory and Compile Graph
+# 7. Build call loop and run it
+# 8. Create tool - DONE
+# 9. Build LLM with tools
+# 10. Create Tool Node
+# 11. Create Router Node
+# 12. Update graph for Tools
+print("Graph initialized with tools.")
