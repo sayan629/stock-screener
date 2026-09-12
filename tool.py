8a70a5d27c30501625369319d9b156c2f9f55210
Sayan Pal <sayanpal771@gmail.com>
2026-09-13 00:23:56 +0530

    Build call loop.

--- tool.py
+++ tool.py
@@ -3,29 +3,31 @@ import yfinance as yf
 import json
 
 @tool
-def simple_screener(screen_type:str, offset:int )-> str:
-    """ Returns screened assets (stocks, funds, bonds) given popular criteria.
+def simple_screener(screen_type: str, offset: int) -> str:
 
-Args:
+    """Returns screened assets (stocks, funds, bonds) given popular criteria.
 
-    screen_type: One of a default set of stock screener queries from Yahoo Finance.
-        aggressive_small_caps
-        day_gainers
-        day_losers
-        growth_technology_stocks
-        most_actives
-        most_shorted_stocks
-        small_cap_gainers
-        undervalued_growth_stocks
-        undervalued_large_caps
-        conservative_foreign_funds
-        high_yield_bond
-        portfolio_anchors
-        solid_large_growth_funds
-        solid_midcap_growth_funds
-        top_mutual_funds
-    offset: the pagination start point
+    Args:
+        screen_type: One of a default set of stock screener queries from yahoo finance.
+            aggressive_small_caps
+            day_gainers
+            day_losers
+            growth_technology_stocks
+            most_actives
+            most_shorted_stocks
+            small_cap_gainers
+            undervalued_growth_stocks
+            undervalued_large_caps
+            conservative_foreign_funds
+            high_yield_bond
+            portfolio_anchors
+            solid_large_growth_funds
+            solid_midcap_growth_funds
+            top_mutual_funds
 
-Returns:
-    A JSON output of assets that meet the criteria.
-"""
+    Returns:
+        The a JSON output of assets that meet the criteria
+    """
+
+    query = yf.PREDEFINED_SCREENER_QUERIES[screen_type]['query']
+    res = yf.screen(query, offset=offset, count=5)
